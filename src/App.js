f7dc2c8ab9682f0962a58ab93129edb67a677f57
Hemal99 <h.githmin@gmail.com>
2022-06-25 11:36:47 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-import logo from "./logo.svg";
-import "./App.css";
 
-const getReviews = () => {
-  const data = fetch(
-    `https://mybusiness.googleapis.com/v4/accounts/6874287982/locations/ChIJNVyMGnhT4joRMj4VSn0hLak/reviews`
-  )
-    .then((response) => response.json())
-    .then((data) => console.log(data));
-};
+import "./App.css";
+import Main from "./Screens/Main";
+
+
 
 function App() {
   return (
     <div className="App">
-      <button onClick={getReviews}>Click me</button>
+      <Main />
     </div>
   );
 }
---
+++ src/Screens/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Carousel from '../Components/Carousel';
+
+
+export default function Main(){
+    return (
+        <div>
+            <Carousel />
+        </div>
+    );
+}
