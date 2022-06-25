021c4208038ce81e63409719943d6cf837381522
Hemal99 <h.githmin@gmail.com>
2022-06-25 16:15:29 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Main from "./Screens/Main";
 import Slides from "./Components/AddSlides/Slides";
-
-
-
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route  path="/add-slides" element={<Slides />} />
       </Routes>
-      <Main />
     </div>
   );
 }
